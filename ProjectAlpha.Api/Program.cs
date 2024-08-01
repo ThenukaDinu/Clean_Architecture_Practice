8ad49cc7af256dd6da4ff314d291366359bd75d9
Thenuka Dinu Jayarathna <thenukadinu@gmail.com>
2024-08-02 00:30:32 +0530

    initial commit

---
+++ ProjectAlpha.Api/Program.cs
@@ -0,0 +1,16 @@
+using ProjectAlpha.Application;
+using ProjectAlpha.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+{
+    builder.Services.AddApplication()
+    .AddInfrastructure();
+    builder.Services.AddControllers();
+}
+
+var app = builder.Build();
+{
+    app.UseHttpsRedirection();
+    app.MapControllers();
+    app.Run();
+}
